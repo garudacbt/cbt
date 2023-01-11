d5eb8182d9ddf05bcabd3e3bbad4a38ee911c8c0
AbangAzmi <abdullahalhasan37@gmail.com>
2023-01-11 22:13:38 +0700

    version 1.3.20230111

--- application/config/database.php
+++ application/config/database.php
@@ -6,10 +6,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
     'dsn'	=> '',
-    'hostname' => 'localhost',
-    'username' => 'root',
+    'hostname' => '',
+    'username' => '',
     'password' => '',
-    'database' => 'elearning',
+    'database' => '',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => FALSE,
