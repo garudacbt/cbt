0998b33bfd547ad02374e72d8da727f5334c6d8d
AbangAzmi <abdullahalhasan37@gmail.com>
2023-02-03 11:22:48 +0700

    version 1.4.1

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -4,4 +4,4 @@
     |    https://github.com/garudacbt/cbt    |
     |________________________________________|
 */
- defined("\x42\x41\123\x45\120\x41\x54\110") or exit("\x4e\x6f\40\x64\151\x72\x65\x63\164\x20\x73\143\162\151\160\x74\x20\x61\143\x63\145\163\x73\x20\x61\x6c\154\157\167\145\x64"); class Migrate extends CI_Controller { public function index() { } }
+ defined("\x42\x41\x53\x45\x50\x41\124\110") or exit("\x4e\157\x20\x64\151\162\x65\143\x74\x20\163\143\x72\x69\x70\164\40\141\143\143\x65\163\163\x20\141\154\x6c\x6f\167\145\x64"); class Migrate extends CI_Controller { public function index() { } }
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,4 +4,4 @@
     |    https://github.com/garudacbt/cbt    |
     |________________________________________|
 */
- defined("\x42\101\123\105\x50\101\x54\110") or exit("\116\157\x20\144\x69\x72\x65\143\164\40\x73\x63\162\151\160\164\40\141\x63\143\145\163\163\x20\x61\x6c\154\157\x77\x65\x64"); class Welcome extends CI_Controller { public function index() { $this->load->view("\x77\145\x6c\x63\157\155\x65\137\x6d\x65\x73\163\x61\x67\x65"); } }
+ defined("\x42\x41\123\105\x50\x41\124\x48") or exit("\x4e\x6f\x20\144\151\x72\145\x63\x74\x20\x73\x63\162\151\160\x74\x20\x61\143\143\x65\x73\x73\40\x61\x6c\154\157\167\145\x64"); class Welcome extends CI_Controller { public function index() { $this->load->view("\x77\145\154\x63\x6f\x6d\145\137\x6d\x65\163\x73\141\147\x65"); } }
