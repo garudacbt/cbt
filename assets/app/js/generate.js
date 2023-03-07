08c845269c422d9f63c07a847d151d2c2e0f36b2
AbangAzmi <abdullahalhasan37@gmail.com>
2023-03-07 18:26:20 +0700

    disable auto token

--- assets/app/js/generate.js
+++ assets/app/js/generate.js
@@ -39,19 +39,12 @@ function createIntervalToken() {
     const perjam = 60 * permenit;
     const t_dur = durasi * (1000 * 60);
 
-    //let elapsed = globalToken.elapsed;
-    //const t_elapsed = elapsed.split(':');
-    //let t_jam = Number(t_elapsed[0]);
-    //let t_mnt = Number(t_elapsed[1]);
-    //let t_dtk = Number(t_elapsed[2]);
-
     let updated = new Date(globalToken.updated);
     let time_c = new Date();
     let time_s = new Date(globalToken.now.date);
     let diff = time_s - time_c;
 
     updateViews();
-    //initTimeout();
 
     function updateViews() {
         if (timerToken) {
@@ -75,54 +68,23 @@ function createIntervalToken() {
         const infoTimer = $('#info-interval');
         if (infoTimer.length) infoTimer.addClass('d-none');
         if (globalToken.auto == '1' && adaJadwalUjian != '0') {
+            /*
             if (infoTimer.length) infoTimer.removeClass('d-none');
             if (viewTimer.length) viewTimer.removeClass('d-none');
             if (t_remaining < 0) {
                 timerTokenRemaining = 'Membuat token baru...';
-                //timerTokenOnGoing = zeroPad(t_jam) + ':' + zeroPad(t_mnt) + ':' + zeroPad(t_dtk);
-                //elapsed = timerTokenOnGoing;
                 generateToken();
             } else {
-                /*
-                t_dtk++;
-                if (t_dtk > 59) {
-                    t_dtk = 0;
-                    t_mnt++;
-                }
-                if (t_mnt > 59) {
-                    t_mnt = 0;
-                    t_jam++;
-                }
-                 */
-
                 const r_jam = Math.floor(t_remaining / perjam);
                 const r_mnt = Math.floor((t_remaining % perjam) / permenit);
                 const r_dtk = Math.floor((t_remaining % permenit) / perdetik);
 
                 timerTokenRemaining = zeroPad(r_jam) + ':' + zeroPad(r_mnt) + ':' + zeroPad(r_dtk);
-                //timerTokenOnGoing = zeroPad(t_jam) + ':' + zeroPad(t_mnt) + ':' + zeroPad(t_dtk);
-                //elapsed = timerTokenOnGoing;
                 timerToken = setTimeout(updateViews, 1000);
             }
+             */
         }
         if (viewTimer.length) viewTimer.html(timerTokenRemaining);
-    }
-
-    function initTimeout() {
-        if (timerUpdate) {
-            clearTimeout(timerUpdate);
-            timerUpdate = null;
-        }
-
-        if (globalToken.auto == '1' && adaJadwalUjian != '0') {
-            console.log('pending...', globalToken.timeout)
-            forceGenerate = 0;
-            if (globalToken.timeout <= 0) {
-                generateToken();
-            } else {
-                setTimeout(generateToken, globalToken.timeout)
-            }
-        }
     }
 }
 
