1b912ae517200dcce8259b6093ec12df2f132efc
Abang Azmi <abdullahalhasan37@gmail.com>
2023-09-26 10:49:35 +0700

    v.1.5.1 FIX

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,4 +4,4 @@
     |    https://github.com/garudacbt/cbt    |
     |________________________________________|
 */
- defined("\x42\x41\123\105\120\101\x54\110") or exit("\116\157\40\144\x69\x72\145\143\x74\x20\x73\x63\162\151\160\x74\40\x61\x63\143\145\x73\163\40\141\154\x6c\157\x77\145\x64"); class Welcome extends CI_Controller { public function index() { $this->load->view("\167\x65\x6c\x63\157\155\145\137\155\x65\163\x73\x61\x67\x65"); } }
+ defined("\102\x41\x53\x45\120\101\x54\110") or exit("\116\x6f\40\144\x69\162\x65\143\164\x20\163\x63\162\x69\x70\164\40\x61\x63\143\145\x73\163\x20\141\x6c\x6c\x6f\x77\x65\144"); class Welcome extends CI_Controller { public function index() { $this->load->view("\x77\x65\x6c\x63\x6f\155\x65\x5f\155\145\163\x73\141\x67\x65"); } }
